f8faeda7d553ecce8aa56a3b7836203412b98a64
RomanNemercev <nemercevroman@gmail.com>
2024-01-26 03:00:04 +0300

    correct tabs-box\добавил табы

--- js/script.js
+++ js/script.js
@@ -14,3 +14,23 @@ document.addEventListener( //когда DOM загрузился
         forcedOriginalScale('noneScalable'); //выполняем функцию, передаём в неё id нашего контейнера
     }
 );
+
+//tabs
+let tabsBtn = document.querySelectorAll('.vacancies__value');
+let tabsItem = document.querySelectorAll('.vacancies__content');
+
+tabsBtn.forEach(function (element) {
+    element.addEventListener('click', function (e) {
+        const path = e.currentTarget.dataset.path;
+
+        tabsBtn.forEach(function (btn) {
+            btn.classList.remove('vacancies__value--active')
+        });
+        e.currentTarget.classList.add('vacancies__value--active')
+
+        tabsItem.forEach(function (element) {
+            element.classList.remove('vacancies__content--active')
+        });
+        document.querySelector(`[data-target="${path}"]`).classList.add('vacancies__content--active');
+    })
+});
