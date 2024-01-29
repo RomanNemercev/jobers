746914c8a81b424093b105474d269c90e5dec0d8
RomanNemercev <nemercevroman@gmail.com>
2024-01-29 22:20:16 +0300

    добавил ф-ии выпадающего меню

--- js/script.js
+++ js/script.js
@@ -34,3 +34,105 @@ tabsBtn.forEach(function (element) {
         document.querySelector(`[data-target="${path}"]`).classList.add('vacancies__content--active');
     })
 });
+
+//drop-down in vacancies-item
+// list-one
+let burger = document.querySelector('.item__burger');
+let burgerList = document.querySelector('.item__burger-list');
+let itemTop = document.querySelector('.item__top');
+let menuLinks = document.querySelectorAll('.item__burger-btn');
+
+burger.addEventListener('click',
+    function () {
+        burgerList.classList.toggle('item__burger-list--active');
+        itemTop.classList.toggle('item__top--active');
+    })
+
+menuLinks.forEach(function (el) {
+    el.addEventListener('click',
+        function () {
+            burgerList.classList.remove('item__burger-list--active');
+            itemTop.classList.remove('item__top--active');
+        })
+})
+
+//list-two
+let burgerTwo = document.querySelector('.item-two');
+let burgerListTwo = document.querySelector('.list-two');
+let itemTopTwo = document.querySelector('.top-two');
+let menuLinksTwo = document.querySelectorAll('.item__btn-two');
+
+burgerTwo.addEventListener('click',
+    function () {
+        burgerListTwo.classList.toggle('item__burger-list--active');
+        itemTopTwo.classList.toggle('item__top--active');
+    })
+
+menuLinksTwo.forEach(function (el) {
+    el.addEventListener('click',
+        function () {
+            burgerListTwo.classList.remove('item__burger-list--active');
+            itemTopTwo.classList.remove('item__top--active');
+        })
+})
+
+//list-third
+let burgerThird = document.querySelector('.item-third');
+let burgerListThird = document.querySelector('.list-third');
+let itemTopThird = document.querySelector('.top-third');
+let menuLinksThird = document.querySelectorAll('.item__btn-third');
+
+burgerThird.addEventListener('click',
+    function () {
+        burgerListThird.classList.toggle('item__burger-list--active');
+        itemTopThird.classList.toggle('item__top--active');
+    })
+
+menuLinksThird.forEach(function (el) {
+    el.addEventListener('click',
+        function () {
+            burgerListThird.classList.remove('item__burger-list--active');
+            itemTopThird.classList.remove('item__top--active');
+        })
+})
+
+//archive block
+//archive-one
+let burgerArchiveOne = document.querySelector('.item__archive-one');
+let burgerListArchiveOne = document.querySelector('.list__archive-one');
+let itemTopArchiveOne = document.querySelector('.archive__top-one');
+let menuLinksArchiveOne = document.querySelectorAll('.archive__btn-one');
+
+burgerArchiveOne.addEventListener('click',
+    function () {
+        burgerListArchiveOne.classList.toggle('item__burger-list--active');
+        itemTopArchiveOne.classList.toggle('item__top--active');
+    })
+
+menuLinksArchiveOne.forEach(function (el) {
+    el.addEventListener('click',
+        function () {
+            burgerListArchiveOne.classList.remove('item__burger-list--active');
+            itemTopArchiveOne.classList.remove('item__top--active');
+        })
+})
+
+//archive-two
+let burgerArchiveTwo = document.querySelector('.item__archive-two');
+let burgerListArchiveTwo = document.querySelector('.list__archive-two');
+let itemTopArchiveTwo = document.querySelector('.archive__top-two');
+let menuLinksArchiveTwo = document.querySelectorAll('.archive__btn-two');
+
+burgerArchiveTwo.addEventListener('click',
+    function () {
+        burgerListArchiveTwo.classList.toggle('item__burger-list--active');
+        itemTopArchiveTwo.classList.toggle('item__top--active');
+    })
+
+menuLinksArchiveTwo.forEach(function (el) {
+    el.addEventListener('click',
+        function () {
+            burgerListArchiveTwo.classList.remove('item__burger-list--active');
+            itemTopArchiveTwo.classList.remove('item__top--active');
+        })
+})
