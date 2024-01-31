9cd41a705a90efc440e278f259b9f701451426c6
RomanNemercev <nemercevroman@gmail.com>
2024-01-31 21:33:20 +0300

    убрал календарь, отключил автозум, отключил автовысоту на блоках вакансий при выпадающем меню, добавил многофункциональный попап на обе страницы(активные+архив)

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-// none-scalable for nav-bar
-function forcedOriginalScale(noneScalable) {
-
-    var App = document.getElementById("noneScalable"); //получаем div по его id
-    App.style.zoom = 1 / devicePixelRatio; //устанавливаем масштаб в зависимости от pixel-ratio
-
-}
-
-document.addEventListener( //когда DOM загрузился
-
-    "DOMContentLoaded",
-
-    function () {
-        forcedOriginalScale('noneScalable'); //выполняем функцию, передаём в неё id нашего контейнера
-    }
-);
+// // none-scalable for nav-bar
+// function forcedOriginalScale(noneScalable) {
+//
+//     var App = document.getElementById("noneScalable"); //получаем div по его id
+//     App.style.zoom = 1 / devicePixelRatio; //устанавливаем масштаб в зависимости от pixel-ratio
+//
+// }
+//
+// document.addEventListener( //когда DOM загрузился
+//
+//     "DOMContentLoaded",
+//
+//     function () {
+//         forcedOriginalScale('noneScalable'); //выполняем функцию, передаём в неё id нашего контейнера
+//     }
+// );
 
 //tabs
 let tabsBtn = document.querySelectorAll('.vacancies__value');
@@ -39,60 +39,51 @@ tabsBtn.forEach(function (element) {
 // list-one
 let burger = document.querySelector('.item__burger');
 let burgerList = document.querySelector('.item__burger-list');
-let itemTop = document.querySelector('.item__top');
 let menuLinks = document.querySelectorAll('.item__burger-btn');
 
 burger.addEventListener('click',
     function () {
         burgerList.classList.toggle('item__burger-list--active');
-        itemTop.classList.toggle('item__top--active');
     })
 
 menuLinks.forEach(function (el) {
     el.addEventListener('click',
         function () {
             burgerList.classList.remove('item__burger-list--active');
-            itemTop.classList.remove('item__top--active');
         })
 })
 
 //list-two
 let burgerTwo = document.querySelector('.item-two');
 let burgerListTwo = document.querySelector('.list-two');
-let itemTopTwo = document.querySelector('.top-two');
 let menuLinksTwo = document.querySelectorAll('.item__btn-two');
 
 burgerTwo.addEventListener('click',
     function () {
         burgerListTwo.classList.toggle('item__burger-list--active');
-        itemTopTwo.classList.toggle('item__top--active');
     })
 
 menuLinksTwo.forEach(function (el) {
     el.addEventListener('click',
         function () {
             burgerListTwo.classList.remove('item__burger-list--active');
-            itemTopTwo.classList.remove('item__top--active');
         })
 })
 
 //list-third
 let burgerThird = document.querySelector('.item-third');
 let burgerListThird = document.querySelector('.list-third');
-let itemTopThird = document.querySelector('.top-third');
 let menuLinksThird = document.querySelectorAll('.item__btn-third');
 
 burgerThird.addEventListener('click',
     function () {
         burgerListThird.classList.toggle('item__burger-list--active');
-        itemTopThird.classList.toggle('item__top--active');
     })
 
 menuLinksThird.forEach(function (el) {
     el.addEventListener('click',
         function () {
             burgerListThird.classList.remove('item__burger-list--active');
-            itemTopThird.classList.remove('item__top--active');
         })
 })
 
@@ -100,40 +91,89 @@ menuLinksThird.forEach(function (el) {
 //archive-one
 let burgerArchiveOne = document.querySelector('.item__archive-one');
 let burgerListArchiveOne = document.querySelector('.item__list__archive-one');
-let itemTopArchiveOne = document.querySelector('.item__archive__top-one');
 let menuLinksArchiveOne = document.querySelectorAll('.item__archive__btn-one');
 
 burgerArchiveOne.addEventListener('click',
     function () {
         burgerListArchiveOne.classList.toggle('item__burger-list--active');
-        itemTopArchiveOne.classList.toggle('item__top--active');
     })
 
 menuLinksArchiveOne.forEach(function (el) {
     el.addEventListener('click',
         function () {
             burgerListArchiveOne.classList.remove('item__burger-list--active');
-            itemTopArchiveOne.classList.remove('item__top--active');
         })
 })
 
 //archive-two
 let burgerArchiveTwo = document.querySelector('.item__archive-two');
 let burgerListArchiveTwo = document.querySelector('.item__list__archive-two');
-let itemTopArchiveTwo = document.querySelector('.item__archive__top-two');
 let menuLinksArchiveTwo = document.querySelectorAll('.item__archive__btn-two');
 
 burgerArchiveTwo.addEventListener('click',
     function () {
         burgerListArchiveTwo.classList.toggle('item__burger-list--active');
-        itemTopArchiveTwo.classList.toggle('item__top--active');
     })
 
 menuLinksArchiveTwo.forEach(function (el) {
     el.addEventListener('click',
         function () {
             burgerListArchiveTwo.classList.remove('item__burger-list--active');
-            itemTopArchiveTwo.classList.remove('item__top--active');
         })
 })
 
+//filter-choice
+const multiSelect = () => {
+    const elements = document.querySelectorAll('.vacancies__select-wrapper');
+    elements.forEach(el => {
+        const choices = new Choices(el, {
+            searchEnabled: false,
+            itemSelectText: '',
+            shouldSort: false,
+        })
+    });
+};
+
+multiSelect();
+
+let vacanciesFilterActive = document.getElementById("filterActive");
+let vacanciesFilterArchive = document.getElementById("filterArchive");
+let vacanciesInput = document.querySelectorAll('.vacancies__input');
+let itemNameInput = document.querySelectorAll('.item__name-input');
+let filterResetBtn = document.querySelectorAll('.vacancies__filter-reset');
+
+vacanciesInput.forEach(function (el) {
+    el.addEventListener('click',
+        function () {
+            vacanciesFilterActive.classList.toggle('vacancies__filter--active');
+        })
+})
+
+vacanciesInput.forEach(function (el) {
+    el.addEventListener('click',
+        function () {
+            vacanciesFilterArchive.classList.toggle('vacancies__filter--active');
+        })
+})
+
+itemNameInput.forEach(function (el) {
+    el.addEventListener('click',
+        function () {
+            vacanciesFilterActive.classList.toggle('vacancies__filter--active');
+        })
+})
+
+itemNameInput.forEach(function (el) {
+    el.addEventListener('click',
+        function () {
+            vacanciesFilterArchive.classList.toggle('vacancies__filter--active');
+        })
+})
+
+filterResetBtn.forEach(function (el) {
+    el.addEventListener('click',
+        function () {
+        vacanciesFilterActive.classList.remove('vacancies__filter--active');
+        vacanciesFilterArchive.classList.remove('vacancies__filter--active');
+        })
+})
