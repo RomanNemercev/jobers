dead2efebb351a66c30c5e8d02f8e76f93988e85
RomanNemercev <nemercevroman@gmail.com>
2024-02-05 16:54:16 +0300

    добавил разметку контактной информации

--- js/script-add-vacancies.js
+++ js/script-add-vacancies.js
@@ -18,3 +18,38 @@ new Accordion('.accordion-list', {
     panelClass: 'accordion__content',
     activeClass: 'accordion--active'
 });
+
+//map
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+
+function init() {
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [55.751590873860096,37.61924179541014],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 12,
+    });
+
+    // var myPlacemark = new ymaps.Placemark([55.751590873860096,37.61924179541014], {}, {
+    //     iconLayout: 'default#image',
+    //     iconImageHref: 'img/map_label.svg',
+    //     iconImageSize: [20, 20],
+    // });
+
+    // myMap.geoObjects.add(myPlacemark);
+    myMap.controls.remove('geolocationControl'); // удаляем геолокацию
+    myMap.controls.remove('searchControl'); // удаляем поиск
+    myMap.controls.remove('trafficControl'); // удаляем контроль трафика
+    myMap.controls.remove('typeSelector'); // удаляем тип
+    myMap.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    myMap.controls.remove('zoomControl'); // удаляем контрол зуммирования
+    myMap.controls.remove('rulerControl'); // удаляем контроль правил
+    myMap.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+}
