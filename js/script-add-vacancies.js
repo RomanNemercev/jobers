efa34aa5653e495a5e8c8adfeec539a3642e0b96
RomanNemercev <nemercevroman@gmail.com>
2024-02-06 17:13:51 +0300

    завершен блок контактная информация, начат блок выбора тарифоф

--- js/script-add-vacancies.js
+++ js/script-add-vacancies.js
@@ -53,3 +53,28 @@ function init() {
     myMap.controls.remove('rulerControl'); // удаляем контроль правил
     myMap.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
 }
+
+//custom btn
+// I already selected these elements and signed them as variables so we can save up some time.
+// You can pause and do the same...
+const img_container = document.querySelector('.add__preview');
+const img = document.querySelector('.add__background');
+const input = document.querySelector('.add__input');
+// const file_name = document.querySelector('.file_name');
+
+input.addEventListener('change', function() {
+    const file = this.files[0];
+
+    if (file) {
+        img_container.style.display = 'block';
+
+        const reader = new FileReader()
+        reader.onload = function () {
+            const result = reader.result;
+            img.src = result;
+
+            file_name.innerText = file.name;
+        }
+        reader.readAsDataURL(file);
+    }
+})
