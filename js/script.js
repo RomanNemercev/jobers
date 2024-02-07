537cd2c52dccd80b9696d9257aa9d836bbf36739
RomanNemercev <nemercevroman@gmail.com>
2024-02-07 15:49:36 +0300

    один из вариантов недо-выпадающего списка в блоке с поиском. С последующим откатом на дефолт значение. Найдено ещё несколько вариантов, тестирование.

--- js/script.js
+++ js/script.js
@@ -42,13 +42,20 @@ let burgerList = document.querySelector('.item__burger-list');
 let inWorkDotsOne = document.querySelectorAll('.dot-inwork-one');
 let menuLinks = document.querySelectorAll('.item__burger-btn');
 
-burger.addEventListener('click',
-    function () {
-        burgerList.classList.toggle('item__burger-list--active');
-        inWorkDotsOne.forEach(function (el) {
-            el.classList.toggle('item__burger-dot--active')
+try {
+    // код...
+    burger.addEventListener('click',
+        function () {
+            burgerList.classList.toggle('item__burger-list--active');
+            inWorkDotsOne.forEach(function (el) {
+                el.classList.toggle('item__burger-dot--active')
+            })
         })
-    })
+} catch (err) {
+    // обработка ошибки
+    console.log('ошибка обработки первого дроплиста')
+}
+
 
 menuLinks.forEach(function (el) {
     el.addEventListener('click',
@@ -66,13 +73,19 @@ let burgerListTwo = document.querySelector('.list-two');
 let inWorkDotsTwo = document.querySelectorAll('.dot-inwork-two');
 let menuLinksTwo = document.querySelectorAll('.item__btn-two');
 
-burgerTwo.addEventListener('click',
-    function () {
-        burgerListTwo.classList.toggle('item__burger-list--active');
-        inWorkDotsTwo.forEach(function (el) {
-            el.classList.toggle('item__burger-dot--active')
+try {
+    // код...
+    burgerTwo.addEventListener('click',
+        function () {
+            burgerListTwo.classList.toggle('item__burger-list--active');
+            inWorkDotsTwo.forEach(function (el) {
+                el.classList.toggle('item__burger-dot--active')
+            })
         })
-    })
+} catch (err) {
+    // обработка ошибки
+    console.log('ошибка обработки второго дроплиста')
+}
 
 menuLinksTwo.forEach(function (el) {
     el.addEventListener('click',
@@ -90,6 +103,8 @@ let burgerListThird = document.querySelector('.list-third');
 let inWorkDotsThree = document.querySelectorAll('.dot-inwork-three');
 let menuLinksThird = document.querySelectorAll('.item__btn-third');
 
+try {
+    // код...
 burgerThird.addEventListener('click',
     function () {
         burgerListThird.classList.toggle('item__burger-list--active');
@@ -97,6 +112,11 @@ burgerThird.addEventListener('click',
             el.classList.toggle('item__burger-dot--active')
         })
     })
+
+} catch (err) {
+    // обработка ошибки
+    console.log('ошибка обработки третьего дроплиста')
+}
 
 menuLinksThird.forEach(function (el) {
     el.addEventListener('click',
@@ -115,6 +135,8 @@ let burgerActiveListOne = document.querySelector('.item__list__active-one');
 let inActiveDotsOne = document.querySelectorAll('.dot-active-one');
 let menuActiveLinksOne = document.querySelectorAll('.item__active__btn-one');
 
+
+try {
 burgerActiveOne.addEventListener('click',
     function () {
         burgerActiveListOne.classList.toggle('item__burger-list--active');
@@ -122,6 +144,9 @@ burgerActiveOne.addEventListener('click',
             el.classList.toggle('item__burger-dot--active')
         })
     })
+} catch (err) {
+    console.log('ошибка обработки четвертого дроплиста')
+}
 
 menuActiveLinksOne.forEach(function (el) {
     el.addEventListener('click',
@@ -139,6 +164,7 @@ let burgerActiveListTwo = document.querySelector('.item__list__active-two');
 let inActiveDotsTwo = document.querySelectorAll('.dot-active-two');
 let menuActiveLinksTwo = document.querySelectorAll('.item__active__btn-two');
 
+try {
 burgerActiveTwo.addEventListener('click',
     function () {
         burgerActiveListTwo.classList.toggle('item__burger-list--active');
@@ -146,6 +172,9 @@ burgerActiveTwo.addEventListener('click',
             el.classList.toggle('item__burger-dot--active')
         })
     })
+} catch (err) {
+    console.log('ошибка обработки пятого дроплиста')
+}
 
 menuActiveLinksTwo.forEach(function (el) {
     el.addEventListener('click',
@@ -164,6 +193,7 @@ let burgerListArchiveOne = document.querySelector('.item__list__archive-one');
 let inArchiveDotsOne = document.querySelectorAll('.dot-archive-one');
 let menuLinksArchiveOne = document.querySelectorAll('.item__archive__btn-one');
 
+try {
 burgerArchiveOne.addEventListener('click',
     function () {
         burgerListArchiveOne.classList.toggle('item__burger-list--active');
@@ -171,6 +201,9 @@ burgerArchiveOne.addEventListener('click',
             el.classList.toggle('item__burger-dot--active')
         })
     })
+} catch (err) {
+    console.log('ошибка обработки шестого дроплиста')
+}
 
 menuLinksArchiveOne.forEach(function (el) {
     el.addEventListener('click',
@@ -188,19 +221,23 @@ let burgerListArchiveTwo = document.querySelector('.item__list__archive-two');
 let inArchiveDotsTwo = document.querySelectorAll('.dot-archive-two');
 let menuLinksArchiveTwo = document.querySelectorAll('.item__archive__btn-two');
 
+try {
 burgerArchiveTwo.addEventListener('click',
     function () {
         burgerListArchiveTwo.classList.toggle('item__burger-list--active');
-         inArchiveDotsTwo.forEach(function (el) {
+        inArchiveDotsTwo.forEach(function (el) {
             el.classList.toggle('item__burger-dot--active')
         })
     })
+} catch (err) {
+    console.log('ошибка обработки седьмого дроплиста')
+}
 
 menuLinksArchiveTwo.forEach(function (el) {
     el.addEventListener('click',
         function () {
             burgerListArchiveTwo.classList.remove('item__burger-list--active');
-             inArchiveDotsTwo.forEach(function (el) {
+            inArchiveDotsTwo.forEach(function (el) {
                 el.classList.remove('item__burger-dot--active')
             })
         })
@@ -262,4 +299,57 @@ filterResetBtn.forEach(function (el) {
         })
 })
 
+//search-dropdown-datalist
+input.onfocus = function () {
+    browsers.style.display = 'block';
+};
+for (let option of browsers.options) {
+    option.onclick = function () {
+        input.value = option.value;
+        browsers.style.display = 'none';
+        input.style.borderRadius = "5px";
+    }
+};
 
+input.oninput = function() {
+    currentFocus = -1;
+    var text = input.value.toUpperCase();
+    for (let option of browsers.options) {
+        if(option.value.toUpperCase().indexOf(text) > -1){
+            option.style.display = "block";
+        }else{
+            option.style.display = "none";
+        }
+    };
+}
+var currentFocus = -1;
+input.onkeydown = function(e) {
+    if(e.keyCode == 40){
+        currentFocus++
+        addActive(browsers.options);
+    }
+    else if(e.keyCode == 38){
+        currentFocus--
+        addActive(browsers.options);
+    }
+    else if(e.keyCode == 13){
+        e.preventDefault();
+        if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (browsers.options) browsers.options[currentFocus].click();
+        }
+    }
+}
+
+function addActive(x) {
+    if (!x) return false;
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    x[currentFocus].classList.add("active");
+}
+function removeActive(x) {
+    for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("active");
+    }
+}
