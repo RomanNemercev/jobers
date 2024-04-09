60ef16f300a908d06998b0c50cba2a85e65a6d4b
RomanNemercev <nemercevroman@gmail.com>
2024-04-10 00:29:47 +0300

    intersave, added list in vacancies and candidates. added preview before print cans. And some edits.

--- js/libs-js/select.js
+++ js/libs-js/select.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $('.droplist__selected').click(function (e) {
-        let cross = $(".droplist__icon_cross");
+        let cross = $( ".droplist__icon_cross" );
 
-        if (!cross.is(e.target)
-            && cross.has(e.target).length === 0) {
+        if ( !cross.is(e.target)
+            && cross.has(e.target).length === 0 ) {
             $(this).closest('.droplist').toggleClass('droplist_active');
         }
 
@@ -12,18 +12,18 @@ $(document).ready(function () {
 
         let currentVal = $(this).text();
 
-        if ($(this).closest('.droplist').hasClass('droplist__select') || $(this).closest('.droplist').hasClass('droplist__check')) {
+        if($(this).closest('.droplist').hasClass('droplist__select') || $(this).closest('.droplist').hasClass('droplist__check')) {
             $(this).closest('.droplist').find('.droplist__result').addClass('droplist__result_active');
         }
 
-        if ($(this).closest('.droplist').hasClass('droplist__select')) {
+        if($(this).closest('.droplist').hasClass('droplist__select')) {
             $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(currentVal);
         }
 
-        if ($(this).closest('.droplist').hasClass('droplist__check')) {
+        if($(this).closest('.droplist').hasClass('droplist__check')) {
 
             $(this).closest('.droplist').find('.droplist__result').addClass();
-            let currentValChecked = $('.droplist__check .check:checked').map(function () {
+            let currentValChecked = $(this).closest('.droplist').find('.check:checked').map(function () {
                 return $(this).val();
             }).get().join(', ');
             $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(currentValChecked);
@@ -32,7 +32,8 @@ $(document).ready(function () {
                 $(this).closest('.droplist').find('.droplist__result').removeClass('droplist__result_active');
                 $(this).closest('.droplist').find('.droplist__result_text').text('Выберите несколько вариантов');
                 $(this).closest('.droplist').find('.droplist__result').removeClass('droplist__result_max');
-            } else {
+            }
+            else {
                 $(this).closest('.droplist').find('.droplist__result').addClass('droplist__result_max');
             }
 
@@ -45,43 +46,45 @@ $(document).ready(function () {
         $(this).closest('.droplist__result').removeClass('droplist__result_active');
         $(this).closest('.droplist').find('.droplist__item').removeClass('droplist__item_active');
         $(this).closest('.droplist').find('.droplist__field').val('');
-        if ($(this).closest('.droplist').hasClass('droplist__select')) {
+        if($(this).closest('.droplist').hasClass('droplist__select')) {
             $(this).closest('.droplist').find('.droplist__result_text').text('Выберите 1 вариант');
         }
-        if ($(this).closest('.droplist').hasClass('droplist__fields')) {
+        if($(this).closest('.droplist').hasClass('droplist__fields')) {
             $(this).closest('.droplist').find('.droplist__result_text').text('Заработная плата');
         }
     });
 
     $('.droplist__field').keyup(function () {
-        let currentFromVal = $('.droplist__field_from').val();
-        let currentToVal = $('.droplist__field_to').val();
+
+        let currentFromVal = $(this).closest('.droplist').find('.droplist__field_from').val();
+        let currentToVal = $(this).closest('.droplist').find('.droplist__field_to').val();
         let fromValResult = 'от ' + currentFromVal.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
         let toValResult = ' до ' + currentToVal.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
         $(this).closest('.droplist').find('.droplist__result').show();
         $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text('');
-        if ($('.droplist__field_from').val() !== '') {
+        if($('.droplist__field_from').val() !== '') {
             $(this).closest('.droplist').find('.droplist__result').addClass('droplist__result_active');
             $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(fromValResult);
         }
-        if ($('.droplist__field_to').val() !== '') {
+        if($('.droplist__field_to').val()  !== '') {
             $(this).closest('.droplist').find('.droplist__result').addClass('droplist__result_active');
             $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(toValResult);
         }
-        if ($('.droplist__field_from').val() !== '' && $('.droplist__field_to').val() !== '') {
+        if($('.droplist__field_from').val() !== '' && $('.droplist__field_to').val()  !== '') {
             $(this).closest('.droplist').find('.droplist__result').addClass('droplist__result_active');
             $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(fromValResult + toValResult);
         }
-        if ($('.droplist__field').val() == '') {
-            $(this).closest('.droplist').children('.droplist__result').removeClass('droplist__result_active');
+        if($('.droplist__field').val() == '') {
+            $(this).closest('.droplist').find('.droplist__result').removeClass('droplist__result_active');
+            $(this).closest('.droplist').find('.droplist__result_text').text('Заработная плата');
         }
 
     });
 
-    $(".droplist").mouseup(function (e) {
-        let drop = $(".form__list");
-        if (!drop.is(e.target)
-            && drop.has(e.target).length === 0) {
+    $(".droplist").mouseup( function(e){
+        let drop = $( ".form__list" );
+        if ( !drop.is(e.target)
+            && drop.has(e.target).length === 0 ) {
             $(this).removeClass('droplist_active');
         }
     });
