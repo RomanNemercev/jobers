44264d1e3b916707e5629f354624a007df658b5c
RomanNemercev <nemercevroman@gmail.com>
2024-06-12 23:50:30 +0300

    finish move sets on tabs layout and create passEye comp. Move mobile sets, than start layout on PP.

--- js/libs-js/select.js
+++ js/libs-js/select.js
@@ -1,60 +1,57 @@
-$(document).ready(function () {
-     let startText;
-
+function dropL() {
     $('.droplist__selected').click(function (e) {
         $('.droplist').not($(this).closest('.droplist')).removeClass('droplist_active');
-        let cross = $( ".droplist__icon_cross" ); 
-        
-        if ( !cross.is(e.target) 
-		    && cross.has(e.target).length === 0 ) { 
-                $(this).closest('.droplist').toggleClass('droplist_active');
-		}
-        
+        let cross = $(".droplist__icon_cross");
+
+        if (!cross.is(e.target)
+            && cross.has(e.target).length === 0) {
+            $(this).closest('.droplist').toggleClass('droplist_active');
+        }
+
     });
     $('.droplist__item').click(function () {
         startText = $(this).closest('.droplist').find('.droplist__result_text').data('start');
         console.log(startText);
         let currentVal = $(this).text();
 
-        if($(this).closest('.droplist').hasClass('droplist__select') || $(this).closest('.droplist').hasClass('droplist__check')) {
+        if ($(this).closest('.droplist').hasClass('droplist__select') || $(this).closest('.droplist').hasClass('droplist__check')) {
             $(this).closest('.droplist').find('.droplist__result').addClass('droplist__result_active');
         }
-       
-       if($(this).closest('.droplist').hasClass('droplist__select')) {
-        $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(currentVal);
-       }
 
-       if($(this).closest('.droplist').hasClass('droplist__check')) {
-
-        $(this).closest('.droplist').find('.droplist__result').addClass();
-        let currentValChecked = $(this).closest('.droplist').find('.check:checked').map(function () {
-            return $(this).val();
-        }).get().join(', ');
-        $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(currentValChecked);
-
-        if (currentValChecked == 0) {
-            $(this).closest('.droplist').find('.droplist__result').removeClass('droplist__result_active');
-            $(this).closest('.droplist').find('.droplist__result_text').text(startText);
-            $(this).closest('.droplist').find('.droplist__result').removeClass('droplist__result_max');
+        if ($(this).closest('.droplist').hasClass('droplist__select')) {
+            $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(currentVal);
         }
-        else {
-            $(this).closest('.droplist').find('.droplist__result').addClass('droplist__result_max');
+
+        if ($(this).closest('.droplist').hasClass('droplist__check')) {
+
+            $(this).closest('.droplist').find('.droplist__result').addClass();
+            let currentValChecked = $(this).closest('.droplist').find('.check:checked').map(function () {
+                return $(this).val();
+            }).get().join(', ');
+            $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(currentValChecked);
+
+            if (currentValChecked == 0) {
+                $(this).closest('.droplist').find('.droplist__result').removeClass('droplist__result_active');
+                $(this).closest('.droplist').find('.droplist__result_text').text(startText);
+                $(this).closest('.droplist').find('.droplist__result').removeClass('droplist__result_max');
+            } else {
+                $(this).closest('.droplist').find('.droplist__result').addClass('droplist__result_max');
+            }
+
         }
-        
-       }
-        
+
         $(this).addClass('droplist__item_active').siblings().removeClass('droplist__item_active');
-        
+
     });
-    $('.droplist__icon_cross').click(function () { 
+    $('.droplist__icon_cross').click(function () {
         startText = $(this).closest('.droplist').find('.droplist__result_text').data('start');
         $(this).closest('.droplist__result').removeClass('droplist__result_active');
         $(this).closest('.droplist').find('.droplist__item').removeClass('droplist__item_active');
         $(this).closest('.droplist').find('.droplist__field').val('');
-        if($(this).closest('.droplist').hasClass('droplist__select')) {
+        if ($(this).closest('.droplist').hasClass('droplist__select')) {
             $(this).closest('.droplist').find('.droplist__result_text').text(startText);
         }
-        if($(this).closest('.droplist').hasClass('droplist__fields')) {
+        if ($(this).closest('.droplist').hasClass('droplist__fields')) {
             $(this).closest('.droplist').find('.droplist__result_text').text(startText);
         }
     });
@@ -67,32 +64,38 @@ $(document).ready(function () {
         $(this).closest('.droplist').find('.droplist__result').show();
         $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text('');
 
-        if(currentFromVal !== '' && currentToVal  == '') {
+        if (currentFromVal !== '' && currentToVal == '') {
             $(this).closest('.droplist').find('.droplist__result').addClass('droplist__result_active');
             $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(fromValResult);
         }
-        if(currentToVal  !== '' && currentFromVal == '') {
+        if (currentToVal !== '' && currentFromVal == '') {
             $(this).closest('.droplist').find('.droplist__result').addClass('droplist__result_active');
             $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(toValResult);
         }
-        if(currentFromVal !== '' && currentToVal  !== '') {
+        if (currentFromVal !== '' && currentToVal !== '') {
             $(this).closest('.droplist').find('.droplist__result').addClass('droplist__result_active');
             $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(fromValResult + toValResult);
         }
-        if(currentFromVal == '' && currentToVal == '') {
+        if (currentFromVal == '' && currentToVal == '') {
             $(this).closest('.droplist').find('.droplist__result').removeClass('droplist__result_active');
             $(this).closest('.droplist').find('.droplist__result_text').text(startText);
         }
-        
+
     });
-     // Использовать если списки в простом блоке
-    $(".droplist").mouseup( function(e){ 
-		let drop = $( ".form__list" );
-		if ( !drop.is(e.target) 
-		    && drop.has(e.target).length === 0 ) { 
-                $(this).removeClass('droplist_active');
-		}
-	});
+    // Использовать если списки в простом блоке
+    $(".droplist").mouseup(function (e) {
+        let drop = $(".form__list");
+        if (!drop.is(e.target)
+            && drop.has(e.target).length === 0) {
+            $(this).removeClass('droplist_active');
+        }
+    });
+}
+
+$(document).ready(function () {
+    let startText;
+
+    dropL();
 
     // Использовать если списки в Popup
     // $('.cabinet__popup').mouseup(function (e) {
@@ -103,16 +106,16 @@ $(document).ready(function () {
     // });
 
     $('.new-can__container').mouseup(function (e) {
-        let drop = $( ".droplist" );
-        if (drop.has(e.target).length === 0){
+        let drop = $(".droplist");
+        if (drop.has(e.target).length === 0) {
             drop.removeClass('droplist_active');
         }
     });
 
 
     $('.item__popup-wrapper').mouseup(function (e) {
-        let drop = $( ".droplist" );
-        if (drop.has(e.target).length === 0){
+        let drop = $(".droplist");
+        if (drop.has(e.target).length === 0) {
             drop.removeClass('droplist_active');
         }
     });
